7ad11476b3fd4488f3d90fa7fe3db51ef206541f
Rakeeb Rajbhandari <rrajbhandari@insulet.com>
2022-06-22 13:04:53 -0400

    Initial commit

---
+++ src/App.kt
@@ -0,0 +1,27 @@
+
+/**
+ *
+ * class Configuration {
+ *      key, // unique identifier
+ *      version, // string version, e.g. "1", "1.1"
+ *      configuration, // json string configuration
+ *      priority
+ *  }
+ *
+ *  enum Priority {
+ *      default,
+ *      cloud
+ *  }
+ *
+ *  input:
+ *  List<Configuration>
+ *
+ *  functions:
+ *  save(List<Configuration>)
+ *  get(): List<Configuration>
+ *
+ */
+
+fun main() {
+
+}
